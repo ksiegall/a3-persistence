573d7a9eb562c9484061ec492f5f10c562223f88
Glitch (fast-spiritual-arrow) <none>
2024-09-19 03:13:17 +0000

    👰🐧 Checkpoint ./index.js:69904408/1052

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ var express = require('express')
 var app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/users', (req, res) => {
   // Code to handle POST requests to the '/users' path
-  const userId = req.params.id; // Access the parameter value
-  res.send(`User with ID: ${userId}`);
+  // (e.g., for creating a new user)
+  res.send('User creation successful!');
+});
+
+app.get('/users/:id', (req, res) => {
+    const userId = req.params.id; // Access the parameter value
+    res.send(`User with ID: ${userId} from routes`);
 });
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next(); // Pass control to the next middleware or route handler
+});
