1ef5bb598516a37eea256f34971e21f865c08b70
Glitch (fast-spiritual-arrow) <none>
2024-09-19 03:01:19 +0000

    🌾☺️ Checkpoint ./index.js:69904408/11518

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
 var express = require('express')
 var app = express()
+const port = 3000
 
-
-var fs = require('fs')
-var _ = require('lodash')
-var users = [{
-  "name":{
-    "first":"kay",
-    "last":"siegall"
-  }
-}]
-
-// // Reading  a JSON file and initiate an Array of Users
-// fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
-//     if (err) throw err
-  
-//     JSON.parse(data).forEach(function (user) {
-//       user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-//       users.push(user)
-//     })
-  
-//   })
-
-app.get('/', function (req, res) {
-    var buffer = ''
-    users.forEach(function(user){
-        buffer += `<a href="/${user.username}" > ${user.name.full} </a> <br>` 
-    })
-    res.send(buffer)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-app.get('/:username', function (req, res) {
-    var username =  req.params.username
-    res.send(username)
-})
+app.post('/users', (req, res) => {
+  // Code to handle POST requests to the '/users' path
+  const userId = req.params.id; // Access the parameter value
+  res.send(`User with ID: ${userId}`);
+});
 
-var server = app.listen(3000, function(){
-    console.log('Server running at http://localhost:' + server.address().port);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
