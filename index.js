b6d918d4a6f179b1562c26681e4ae3cf48b8e5a6
Glitch (fast-spiritual-arrow) <none>
2024-09-19 03:37:17 +0000

    🐮🎉 Checkpoint ./index.js:69904408/400

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ var express = require('express')
 var app = express()
 const port = 3000
 
+const appdata = [
+  { name: "Kay", score: 132, date: (new Date("July 6, 2023 1:23:45 AM")).toDateString() },
+  { name: "Taylor", score: 42, date: (new Date("September 8, 2024 2:30:00 PM")).toDateString() },
+  { name: "test", score: 1, date: (new Date()).toDateString()},
+];
 
 app.get('/', (req, res) => {
+  res.send('Hello World!')
+//   sendFile(response, "public/index.html");
+})
+
+app.get('/data', (req, res) => {
   res.send('Hello World!')
 //   sendFile(response, "public/index.html");
 })
