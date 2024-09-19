295884e4c9b54ab47795af99e47ef710b151fec4
Glitch (fast-spiritual-arrow) <none>
2024-09-19 02:50:17 +0000

    🖇👘 Checkpoint ./package.json:69904408/212 ./index.js:69904408/94

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ var app = express()
 
 var fs = require('fs')
 var _ = require('lodash')
-var users = []
+var users = [{
+  "name":{
+    "first":"kay",
+    "last":"siegall"
+  }
+}]
 
-// Reading  a JSON file and initiate an Array of Users
-fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
-    if (err) throw err
+// // Reading  a JSON file and initiate an Array of Users
+// fs.readFile('users.json', {encoding: 'utf8'}, function (err, data) {
+//     if (err) throw err
   
-    JSON.parse(data).forEach(function (user) {
-      user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-      users.push(user)
-    })
+//     JSON.parse(data).forEach(function (user) {
+//       user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+//       users.push(user)
+//     })
   
-  })
+//   })
 
 app.get('/', function (req, res) {
     var buffer = ''
