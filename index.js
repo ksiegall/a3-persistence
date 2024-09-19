d87535443eef3a0cc83e97a3d4ca36398d66a0ed
Glitch (fast-spiritual-arrow) <none>
2024-09-19 04:12:17 +0000

    ✈️🏹 Checkpoint ./index.js:69904408/2345

--- index.js
+++ index.js
@@ -8,16 +8,73 @@ const appdata = [
   { name: "test", score: 1, date: (new Date()).toDateString()},
 ];
 
-const sendFileOptions = {root:"."}
+const sendFileOptions = {root:"public/"}
 
 app.get('/', (req, res) => {
   res.sendFile("index.html", sendFileOptions);
 })
-
-app.get('/data', (req, res) => {
-  res.send('Hello World!')
-//   sendFile(response, "public/index.html");
+app.get("/css/main.css", (req, res) => {
+  res.sendFile("/css/main.css", sendFileOptions);
 })
+app.get("/js/main.js", (req, res) => {
+  res.sendFile("/js/main.js", sendFileOptions);
+})
+app.get("/js/snake.js", (req, res) => {
+  res.sendFile("/js/snake.js", sendFileOptions);
+})
+
+app.post("/submit", function() {
+  console.log(JSON.parse(dataString));
+  const data = JSON.parse(dataString);
+  let updated = false;
+  for (var i = 0; i < appdata.length; i++) {
+    if (data.name === appdata[i].name) {
+      appdata[i].score = data.score;
+      updated = true;
+    }
+  }
+});
+// const handlePost = function (request, response) {
+//   let dataString = "";
+
+//   request.on("data", function (data) {
+//     dataString += data;
+//   });
+
+//   request.on("end", function () {
+//     if (request.url === "/submit") {
+//       console.log(JSON.parse(dataString));
+//       const data = JSON.parse(dataString);
+//       let updated = false;
+//       for (var i = 0; i < appdata.length; i++) {
+//         if (data.name === appdata[i].name) {
+//           appdata[i].score = data.score;
+//           updated = true;
+//         }
+//       }
+//       if (!updated) {
+//         console.log("name " + data.name);
+//         appdata.push({ name: data.name, score: data.score, date: (new Date()).toDateString() });
+//       }
+
+//       sortAndSend(request, response);
+//     } else if (request.url === "/delete") {
+//       const data = JSON.parse(dataString);
+//       console.log("Received delete request for " + dataString);
+//       let idx = undefined;
+//       for (let i = 0; i < appdata.length; i++) {
+//         if (appdata[i].name === data.name) {
+//           idx = i;
+//         }
+//       }
+//       if (idx != undefined) {
+//         console.log("Deleting.");
+//         appdata.splice(idx, 1);
+//       }
+//       sortAndSend(request, response);
+//     }
+//   });
+// };
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
